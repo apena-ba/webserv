43f710b4bcc74209561c96d075e57ebdd6ecad05
Emile Fournout <efournou@c3r7s3.42malaga.com>
2023-04-20 18:26:31 +0200

    push emile

--- src/ConfigurationParser.cpp
+++ src/ConfigurationParser.cpp
@@ -62,49 +62,51 @@ void _removeSpace(std::string & str){
     }
     str = result;
 }
-
 bool ConfigurationParser::_checkServer(std::ifstream & file){
     std::string line;
     std::vector <std::string> splitted;
+
+    std::getline(file, line, '{');
+    if (std::find_first_of(line, std::isspace) != line.end())
+    {    throw BadFile("problem name server");}
+    splitted = _split(line, ":");
+}
+bool ConfigurationParser::_checkServer(std::ifstream & file){
+    std::string line;
+    std::vector <std::string> splitted;
+    if (_checkTitle(file) == false){return false;}
     for (unsigned int index = 0; index < 5; index++){
         
         _removeSpace(line);
 
-        file >> line;
-        std::cout << line << std::endl;
+        std::getline(file, line, '\n');
+        std::cout << line;
         splitted = _split(line, ":");
         switch (index){
             case 0:
-                if (splitted.size() != 1 || line[line.size() - 1] != '{')
-                    {
-                        std::cout << line << std::endl;
-                        throw BadFile("problem name");
-                    }
-                break;
-            case 1:
                 if (splitted[0] != "port" || splitted.size() != 2 ||
                 !this->_strIsDigit(splitted[1]) || std::stoi(splitted[1]) < 1024 || std::stoi(splitted[1]) > 65535)
                     {
                         throw BadFile("problem port");
                     }
                 break;
-            case 2:
+            case 1:
                 if (splitted[0] != "default_error_page" || splitted.size() != 2)
                     throw BadFile("problem default error page");
                 break;
-            case 3:
+            case 2:
                 if (splitted[0] != "client_body_size_max" || splitted.size() != 2 || !this->_strIsDigit(splitted[1]))
                     throw BadFile("problem client body size max");
                 break;
-            case 4:
+            case 3:
                 if (splitted[0] != "max_clients" || splitted.size() != 2)
                     throw BadFile("problem max clients");
                 break;
-            case 5:
+            case 4:
                 if (splitted[0] != "routes" || splitted.size() != 2 || !this->_checkRoutes(file))
                     throw BadFile("problem routes");
                 break;
-            case 6:
+            case 5:
                 if (splitted[0] != "}" || splitted.size() != 2)
                     throw BadFile("problem }");
                 break;
