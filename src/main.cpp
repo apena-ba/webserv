22668c52e815f6228e23fa542490b165b1b8e82a
emile_frn <emile.fournout@gmail.com>
2023-05-05 17:25:10 +0200

    change main for merge

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apena-ba <apena-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 17:49:10 by apena-ba          #+#    #+#             */
+/*   Updated: 2023/05/02 19:02:25 by apena-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cluster.hpp"
+#include "ConfigurationParser/utils/ParsingUtils.hpp"
+#include "ConfigurationParser/ConfigurationParser.hpp"
+
+int main(int argc, char **argv, char **env) {
+    try {
+        ConfigurationParser parser;
+        std::vector<Configuration> configs = parser.parse("clean_conf");
+        Cluster cluster(configs);
+        cluster.run();
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return (1);
+    }
+    return (0);
+}
