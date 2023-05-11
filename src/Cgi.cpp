e303c08dc4768b5ac4ee0e23617d2d2a86203168
emile_frn <emile.fournout@gmail.com>
2023-05-11 18:52:20 +0200

    push

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -8,38 +8,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-std::string Cgi::_findScript(const std::string & path){
+std::string Cgi::_findScript(const std::string & path) {
     return path.substr(path.find_last_of('/') + 1);
 }
 
+static std::string _replaceAllOccurence(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+    return str;
+}
+
 std::string Cgi::process(const HTTPRequestParser &request, const Configuration &config) {
-
-    /*for (std::map<std::string, std::string>::const_iterator it = request.getVals().begin(); it != request.getVals().end(); ++it)
-    {
-        std::cout << it->first << ": " << it->second << std::endl;
-    }*/
-
-    std::string phpcgi = "cgi_tester";
+    std::string phpcgi = config.cgi;
     std::string script = _findScript(request.get("location"));
-    std::vector<const char *> env;
+    std::vector<std::string> env;
     std::string request_method = "REQUEST_METHOD=" + request.get("type");
     std::string server_protocol = "SERVER_PROTOCOL=" + request.get("version");
     std::string path_info = "PATH_INFO=" + request.get("location");
-    //TODO: QUERY_STRING
 
-    //env.push_back(redirect_status.c_str());
-    env.push_back(request_method.c_str());
-    //env.push_back(script_filename.c_str());
-    //env.push_back(script_name.c_str());
-    //env.push_back(content_type.c_str());
-    //env.push_back(content_length.c_str());
-    //env.push_back(document_root.c_str());
-    //env.push_back(query_string.c_str());
-    env.push_back(server_protocol.c_str());
-    env.push_back(path_info.c_str());
-    env.push_back(NULL);
-    for (size_t i = 0; env[i] != NULL; i++)
-        std::cout << env[i] << std::endl;
+    env.push_back(request_method);
+    env.push_back(server_protocol);
+    env.push_back(path_info);
+
+    std::string env_var;
+    for (std::map<std::string, std::string>::const_iterator it = request.getVals().begin(); it != request.getVals().end(); ++it)
+    {
+        std::cout << "request: " << it->first << ": " << it->second << std::endl;
+        env_var = _replaceAllOccurence(env_var, "-", "_");
+        env.push_back(env_var);
+    }
+
     std::vector<const char *> argv;
     argv.push_back(phpcgi.c_str());
     argv.push_back("-q");
@@ -65,6 +66,11 @@ std::string Cgi::process(const HTTPRequestParser &request, const Configuration &
         return "";
     }
     if (pid == 0) {
+        char * envp[env.size() + 1];
+        for (size_t i = 0; i < env.size(); i++) {
+            envp[i] = const_cast<char*>(env[i].c_str());
+        }
+        envp[env.size()] = NULL;
         close(stdin_pipefd[1]);
         dup2(stdin_pipefd[0], STDIN_FILENO);
         close(stdin_pipefd[0]);
@@ -73,12 +79,10 @@ std::string Cgi::process(const HTTPRequestParser &request, const Configuration &
         dup2(stdout_pipefd[1], STDOUT_FILENO);
         close(stdout_pipefd[1]);
 
-        execve(argv[0], (char * const *)argv.data(), (char * const*)env.data());
+        execve(argv[0], (char * const *)argv.data(), envp);
         std::cerr << "execve failed" << std::endl;
         std::cerr << "code d'erreur: " << errno << std::endl;
         std::cerr << "erreur: " << strerror(errno) << std::endl;
-        //std::cout << "script_name: " << script_name << std::endl;
-        //std::cout << "script_name: " << script_filename << std::endl;
         return "";
     } else {
         close(stdin_pipefd[0]);
@@ -93,8 +97,6 @@ std::string Cgi::process(const HTTPRequestParser &request, const Configuration &
         }
         close(stdout_pipefd[0]);
         wait(NULL);
-        std::cout << "response: " << response << std::endl;
         return response;
     }
-
 }
