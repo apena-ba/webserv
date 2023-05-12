b26cf143e89d93e6b603890d69343f51f224e79a
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-05-12 16:04:10 +0200

    Maybe now? Brain's not working atm.

--- src/HTTPResponse.cpp
+++ src/HTTPResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:04:02 by ntamayo-          #+#    #+#             */
-/*   Updated: 2023/05/12 15:54:00 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/05/12 16:03:02 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,8 +134,9 @@ void	HTTPResponse::get_perform(const Configuration &conf)
 
 void	HTTPResponse::pos_perform(const Configuration &conf)
 {
-	if (!read_file(conf))
-		return;
+	/* if (!read_file(conf))
+	 *     return; */
+	this->_body = this->_vals["body"];
 	std::string	cgistr = run_cgi(conf);
 	if (this->_vals["location"].back() == '/')
 	{
